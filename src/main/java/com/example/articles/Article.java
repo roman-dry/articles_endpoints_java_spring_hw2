85ee9a102c9674435c06c57697472827a6dfbd2a
roman-dry <romabop39@gmail.com>
2023-04-09 16:01:13 -0400

    CRUD

--- src/main/java/com/example/articles/Article.java
+++ src/main/java/com/example/articles/Article.java
@@ -1,8 +1,9 @@
 package com.example.articles;
 
+import jakarta.validation.constraints.Size;
 
 public class Article {
-
+    @Size(min=20, max=200, message="The article must be at lest 20 chars and no more than 200 chars")
     private String article;
 
     public String getArticle() {
