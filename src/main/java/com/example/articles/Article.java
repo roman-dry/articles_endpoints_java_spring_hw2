2c3e2ed79452eec5b81a082256593b2e2c2eec24
roman-dry <romabop39@gmail.com>
2023-04-12 11:00:45 -0400

    lombok data

--- src/main/java/com/example/articles/Article.java
+++ src/main/java/com/example/articles/Article.java
@@ -1,7 +1,8 @@
 package com.example.articles;
 
 import jakarta.validation.constraints.Size;
-
+import lombok.Data;
+@Data
 public class Article {
     @Size(min=20, max=200, message="The article must be at lest 20 chars and no more than 200 chars")
     private String article;
